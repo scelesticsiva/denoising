e090b6f64a4d6a544e7c444a6eefd1f1571aaff6
Sivaramakrishnan Sankarapandian <sivark@calicolabs.com>
2024-02-01 17:37:23 -0800

    Mixing with repaints

--- inference/diffusion_inference_w_mixing_clean.py
+++ inference/diffusion_inference_w_mixing_clean.py
@@ -19,18 +19,18 @@ from inference.data import npz_dataset
 from inference.val_utils import filter_val_files
 
 class DiffusionSampler(BaseSampler):
-    def sample_func(self, mixing_img_batch, noise=None, start_timesteps=None, bs=4, num_images=1000):
+    def sample_func(self, mixing_img_batch, n_repaint=1, noise=None, start_timesteps=None, bs=4, num_images=1000):
         print('Begining sampling:')
         device = mixing_img_batch.device
 
         # Mixing time steps in reverse direction
         start_time = 100
-        end_time = 50
+        end_time = 20
 
         # print(np.arange(end_time, start_time, 1)[::-1], device)
         time_steps_to_mix = list(range(end_time, start_time, 1))[::-1]
         # const_weights = torch.from_numpy(np.array([0.5] * len(time_steps_to_mix))).to(device)
-        weights = torch.from_numpy(np.linspace(0.8, 0.01, len(time_steps_to_mix))).to(device)
+        weights = torch.from_numpy(np.linspace(0.3, 0.01, len(time_steps_to_mix))).to(device)
         mixing_weights = dict(zip(time_steps_to_mix, weights))
 
         h = w = self.configs.im_size
@@ -73,6 +73,7 @@ class DiffusionSampler(BaseSampler):
                         mixing_img=mixing_img_batch,
                         time_steps_to_mix=time_steps_to_mix,
                         mixing_weights=mixing_weights,
+                        n_repaint=n_repaint,
                         )
             sample = util_image.normalize_th(sample, reverse=True).clamp(0.0, 1.0)
         return sample
@@ -112,7 +113,7 @@ def main(cfg):
     n_batches = int(math.ceil(len(all_data_to_infer) / cfg.bsz))
     master_pred = []
 
-    st = time.time()
+    inference_st = time.time()
     for b_idx in range(n_batches):
         from_ = (b_idx * cfg.bsz)
         to_ = ((b_idx + 1) * cfg.bsz)
@@ -127,7 +128,7 @@ def main(cfg):
         B, C, H, W = curr_batch_clean.shape
         curr_batch_clean = np.reshape(curr_batch_clean, (B * C, 1, H, W))  # Bx1xHxW
 
-        st = time.time()
+        batch_st = time.time()
         y0 = transform(torch.from_numpy(curr_batch).to(device))
         x0_clean = transform(torch.from_numpy(curr_batch_clean).to(device))
 
@@ -140,6 +141,7 @@ def main(cfg):
                 start_timesteps=tN,
                 bs=len(y0),
                 num_images=len(y0),
+                n_repaint=cfg.n_repaint,
             )
 
             if master_curr_pred is None:
@@ -153,11 +155,12 @@ def main(cfg):
 
         if (b_idx+1) % 20 == 0:
             print(f'{b_idx + 1} batches done')
+            print(f'Last batch took {time.time() - batch_st:.4f} seconds')
     
     to_save = np.concatenate(master_pred, axis=0)
     save_fpth = cfg.save_fpth.replace('.npz', f'_t{cfg.tN}_ns{cfg.n_samples}.npz')
     np.savez_compressed(save_fpth, pred=to_save)
-    print(f'Saved pred to {save_fpth}, shape of pred: {to_save.shape}, time: {time.time() - st:.4f} seconds')
+    print(f'Saved pred to {save_fpth}, shape of pred: {to_save.shape}, time: {time.time() - inference_st:.4f} seconds')
 
 if __name__ == '__main__':
     from munch import Munch
@@ -168,13 +171,14 @@ if __name__ == '__main__':
         tN=100, 
         noisy_img_npz='/home/sivark/supporting_files/denoising/data/planaria/Denoising_Planaria/train_data/data_label.npz',
         clean_img_npz='/home/sivark/supporting_files/denoising/data/planaria/Denoising_Planaria/Train2TrainValTest_pred/n2v_pred_Train2TrainValTest_val_idxv2.npz',
-        save_fpth='/home/sivark/supporting_files/denoising/data/planaria/Denoising_Planaria/Train2TrainValTest_pred/Diff_mixing_clean_pred_Train2TrainValTest_val_idx_afterbugfix.npz',
+        save_fpth='/home/sivark/supporting_files/denoising/data/planaria/Denoising_Planaria/Train2TrainValTest_pred/Diff_mixing_clean_pred_Train2TrainValTest_val_idx_afterbugfix_optimal_hparams_repaint.npz',
         key_in_npz='X',
         key_in_npz_clean='pred',
         split_fpth='/home/sivark/supporting_files/denoising/trained_models/planaria_Train2TrainValTestv2.pkl',  # Only required when predicting starting from raw data
         split_to_infer='val_idx',  # Only required when predicting starting from raw data
         bsz=8,
-        n_samples=5,
+        n_samples=1,
+        n_repaint=5,
     ))
 
     main(config)
